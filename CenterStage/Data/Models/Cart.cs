333965e103898a14f2bda1d2983c42af364aa5df
Kristen Shea <kstar61@live.com>
2019-03-07 21:03:38 -0600

    .

---
+++ CenterStage/Data/Models/Cart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CenterStage.Data.Models
+{
+    public class Cart
+    {
+        //public int ID { get; set; }
+
+        //public string CartID { get; set; }
+
+        //public string Title { get; set; }
+
+        //[Display(Name = "Students Registered")]
+        //public int NumberStudentsRegistered { get; set; }
+
+        //[DataType(DataType.Currency), RegularExpression(@"\d{1,20}(\.\d{1,2})?")]
+        //public decimal Price { get; set; }
+
+        //public decimal TotalPrice { get; set; }
+
+
+
+    }
+}
