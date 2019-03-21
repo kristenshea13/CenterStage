f65238b911fb4a0371fb2b5cf0de7c13a4fca847
Kristen Shea <kstar61@live.com>
2019-03-20 19:06:27 -0500

    /

---
+++ CenterStage/Pages/ClassContactList/Delete.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CenterStage.Data;
+using CenterStage.Data.Models;
+
+namespace CenterStage.Pages.ClassContactList
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly CenterStage.Data.ApplicationDbContext _context;
+
+        public DeleteModel(CenterStage.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public StudentRegistration StudentRegistration { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            StudentRegistration = await _context.StudentRegistration
+                .Include(s => s.Class)
+                .Include(s => s.Student).FirstOrDefaultAsync(m => m.ID == id);
+
+            if (StudentRegistration == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            StudentRegistration = await _context.StudentRegistration.FindAsync(id);
+
+            if (StudentRegistration != null)
+            {
+                _context.StudentRegistration.Remove(StudentRegistration);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ CenterStage/Pages/ClassContactList/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CenterStage.Data;
+using CenterStage.Data.Models;
+
+namespace CenterStage.Pages.ClassContactList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CenterStage.Data.ApplicationDbContext _context;
+
+        public IndexModel(CenterStage.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<StudentRegistration> StudentRegistration { get;set; }
+
+
+
+        public async Task OnGetAsync()
+        {
+            StudentRegistration = await _context.StudentRegistration
+                .Include(s => s.Class)
+                .Include(s => s.Student).ToListAsync();
+        }
+    }
+}
