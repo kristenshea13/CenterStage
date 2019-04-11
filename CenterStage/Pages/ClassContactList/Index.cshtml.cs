c6d6017df3eb757fd3d8f2642195a1948fb27075
Kristen Shea <kstar61@live.com>
2019-04-10 19:44:44 -0500

    /

--- CenterStage/Pages/ClassContactList/Index.cshtml.cs
+++ CenterStage/Pages/ClassContactList/Index.cshtml.cs
@@ -1,7 +1,10 @@
 ﻿using CenterStage.Data.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CenterStage.Pages.ClassContactList
@@ -17,35 +20,61 @@ namespace CenterStage.Pages.ClassContactList
 
         public IList<StudentRegistration> StudentRegistration { get; set; }
 
-
-
         //[BindProperty(SupportsGet = true)]
         //public string SearchString { get; set; }
-
-        //public SelectList Titles { get; set; }
+        //// Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        //public SelectList Genres { get; set; }
         //[BindProperty(SupportsGet = true)]
-        //public string Title { get; set; }
+        //public string MovieGenre { get; set; }
 
-        //public async Task OneGetAsync()
-        //{
-        //    var titles = from s in _context.StudentRegistration select s;
+        public SelectList Classes { get; set; }
 
-        //    if (!string.IsNullOrEmpty(SearchString))
-        //    {
-        //        titles = titles.Where(t => t.Class.Title.Contains(SearchString));
-        //    }
+        [BindProperty(SupportsGet = true)]
+        public string ClassTitle { get; set; }
 
-        //    Title = await titles.ToListAsync();
+        //        IQueryable<string> genreQuery = from m in _context.Movie
+        //                                        orderby m.Genre
+        //                                        select m.Genre;
 
-        //}
+        //        var movies = from m in _context.Movie
+        //                     select m;
+
+        //            if (!string.IsNullOrEmpty(SearchString))
+        //            {
+        //                movies = movies.Where(s => s.Title.Contains(SearchString));
+        //            }
+
+        //            if (!string.IsNullOrEmpty(MovieGenre))
+        //            {
+        //                movies = movies.Where(x => x.Genre == MovieGenre);
+        //            }
+        //Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+        //Movie = await movies.ToListAsync();
 
         public async Task OnGetAsync()
         {
             StudentRegistration = await _context.StudentRegistration
                 .Include(s => s.Class)
                 .Include(s => s.Student).ToListAsync();
+
+            //IQueryable<string> genreQuery = (IQueryable<string>)(from c in _context.StudentRegistration
+            //                                orderby c.Class
+            //                                select c.Class);
+
+            //var classes = from c in _context.StudentRegistration
+            //             select c;
+
+
+
+            //if (!string.IsNullOrEmpty(ClassTitle))
+            //{
+            //    classes = classes.Where(x => x.Class.ToString() == ClassTitle);
+            //}
+
+            //StudentRegistration = await classes.ToListAsync();
+
+            //StudentRegistration = await _context.StudentRegistration.Include(c => c.Student).ToListAsync();
+
         }
-
-
     }
 }
