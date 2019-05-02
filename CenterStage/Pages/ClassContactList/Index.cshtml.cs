a3c63d5677ed9af293dd37f1aae05ee515ffec48
Kristen Shea <kstar61@live.com>
2019-05-01 19:56:30 -0500

    /

--- CenterStage/Pages/ClassContactList/Index.cshtml.cs
+++ CenterStage/Pages/ClassContactList/Index.cshtml.cs
@@ -20,22 +20,61 @@ namespace CenterStage.Pages.ClassContactList
 
         public IList<StudentRegistration> StudentRegistration { get; set; }
 
-        
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
 
-        public SelectList Classes { get; set; }
+        public SelectList Class { get; set; }
 
         [BindProperty(SupportsGet = true)]
         public string ClassTitle { get; set; }
 
-        
+        //public IList<Movie> Movie { get; set; }
+        //[BindProperty(SupportsGet = true)]
+        //public string SearchString { get; set; }
+        //// Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        //public SelectList Genres { get; set; }
+        //[BindProperty(SupportsGet = true)]
+        //public string MovieGenre { get; set; }
+
+
+
+
+
+
+
+
 
         public async Task OnGetAsync()
         {
+
+            //var classes = from c in _context.Class select c;
+
+            //if (!string.IsNullOrEmpty(SearchString))
+            //{
+            //    classes = classes.Where(s = s.Class.Contains(SearchString));
+            //}
+
+            //Class = await classes.ToListAsync().ConfigureAwait(false);
+
+
             StudentRegistration = await _context.StudentRegistration
                 .Include(s => s.Class)
                 .Include(s => s.Student).ToListAsync();
 
-            
+
+
+            //    var movies = from m in _context.Movie
+            //                 select m;
+            //    if (!string.IsNullOrEmpty(SearchString))
+            //    {
+            //        movies = movies.Where(s => s.Title.Contains(SearchString));
+            //    }
+
+            //    Movie = await movies.ToListAsync();
+
+
+
+
 
         }
     }
