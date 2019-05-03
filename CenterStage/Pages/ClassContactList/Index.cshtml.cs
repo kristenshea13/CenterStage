e3fb0aca5e5b77e751da2973670a8d61344dfb47
Kristen Shea <kstar61@live.com>
2019-05-02 21:35:03 -0500

    /

--- CenterStage/Pages/ClassContactList/Index.cshtml.cs
+++ CenterStage/Pages/ClassContactList/Index.cshtml.cs
@@ -16,8 +16,6 @@ namespace CenterStage.Pages.ClassContactList
         public IndexModel(CenterStage.Data.ApplicationDbContext context)
         {
             _context = context;
-
-            
         }
 
         public IList<StudentRegistration> StudentRegistration { get; set; }
@@ -25,33 +23,20 @@ namespace CenterStage.Pages.ClassContactList
         [BindProperty(SupportsGet = true)]
         public int? ClassID { get; set; }
 
-        public SelectList ClassTitle { get; set; }    
-
-        
-
-     
-
-
+        public SelectList ClassTitle { get; set; }
 
         public async Task OnGetAsync()
         {
-
             var classes = from c in _context.Class
                           select c;
 
             ClassTitle = new SelectList(classes.ToList(), "ID", "Title");
 
-            
-
             StudentRegistration = await _context.StudentRegistration
                 .Include(s => s.Class)
-                .Include(s => s.Student).Where(s => ClassID.HasValue && 
+                .Include(s => s.Student).Where(s => ClassID.HasValue &&
                 (ClassID == 0 || s.ClassID == ClassID.Value))
                 .OrderBy(s => s.Class.Title).ToListAsync();
-
-
-
-
         }
     }
 }
