f47efe9786d2c704d8940f8bc7b952271e4b1a81
Kristen Shea <kstar61@live.com>
2019-05-08 19:09:32 -0500

    /

---
+++ CenterStage/Data/Migrations/20190509000606_user stuff.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CenterStage.Data.Migrations
+{
+    public partial class userstuff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "StudentInfo",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "StudentInfo",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
